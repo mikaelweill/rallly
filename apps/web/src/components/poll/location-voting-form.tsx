4baa50021ab4a4401ae5d3e72e12d6174e4d60e6
Mikael Weill <weillmikael@gmail.com>
2025-02-17 21:40:49 -0600

    UI is now better for voting locations

--- apps/web/src/components/poll/location-voting-form.tsx
+++ apps/web/src/components/poll/location-voting-form.tsx
@@ -8,13 +8,18 @@ import { Icon } from "@rallly/ui/icon";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@rallly/ui/tooltip";
 import { PlusIcon } from "lucide-react";
 import { Trans } from "next-i18next";
+import { cn } from "@rallly/ui";
+import { MapPinIcon } from "lucide-react";
 
+import { OptimizedAvatarImage } from "@/components/optimized-avatar-image";
 import { useVotingForm } from "@/components/poll/voting-form";
 import { usePoll } from "@/components/poll-context";
 import { usePermissions } from "@/contexts/permissions";
 import { useParticipants } from "@/components/participants-provider";
 
 import LocationOption from "./location-option";
+import VoteSelector from "@/components/poll/vote-selector";
+import VoteIcon from "@/components/poll/vote-icon";
 
 export interface LocationVotingFormProps {
     editable?: boolean;
@@ -74,47 +79,100 @@ const LocationVotingForm: React.FunctionComponent<LocationVotingFormProps> = ({
                     ) : null}
                 </div>
             </CardHeader>
-            <div className="divide-y p-4">
-                {poll.locations.map((location, index) => {
-                    const participants = getParticipantsWhoVotedForOption(location.id);
-                    const score = getScore(location.id);
+            <div className="relative">
+                <div className="scrollbar-thin hover:scrollbar-thumb-gray-400 scrollbar-thumb-gray-300 scrollbar-track-gray-100 relative z-10 flex-grow overflow-auto scroll-smooth">
+                    <table className="w-full table-auto border-separate border-spacing-0 bg-gray-50">
+                        <thead>
+                            <tr>
+                                <th className="sticky left-0 z-30 w-[240px] bg-white pl-4 pr-4">
+                                    <div className="text-sm font-medium">Locations</div>
+                                </th>
+                                {participants.map((participant) => (
+                                    <th
+                                        key={participant.id}
+                                        className="h-12 min-w-[80px] border-l bg-gray-50 px-2"
+                                    >
+                                        <div className="flex flex-col items-center gap-2">
+                                            <OptimizedAvatarImage size="xs" name={participant.name} />
+                                            <div className="truncate text-xs font-normal">
+                                                {participant.name}
+                                            </div>
+                                        </div>
+                                    </th>
+                                ))}
+                                <th className="w-full min-w-4 border-l"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {poll.locations.map((location, index) => (
+                                <Controller
+                                    key={location.id}
+                                    control={control}
+                                    name={`locationVotes.${index}`}
+                                    render={({ field }) => {
+                                        const vote =
+                                            !editable && selectedParticipant
+                                                ? getVote(selectedParticipant.id, location.id)
+                                                : field.value?.type;
 
-                    return (
-                        <Controller
-                            key={location.id}
-                            control={control}
-                            name={`locationVotes.${index}`}
-                            render={({ field }) => {
-                                const vote =
-                                    !editable && selectedParticipant
-                                        ? getVote(selectedParticipant.id, location.id)
-                                        : field.value?.type;
+                                        const handleChange = (newVote: VoteType) => {
+                                            if (!editable) {
+                                                return;
+                                            }
+                                            field.onChange({ locationId: location.id, type: newVote });
+                                        };
 
-                                const handleChange = (newVote: VoteType) => {
-                                    if (!editable) {
-                                        return;
-                                    }
-                                    field.onChange({ locationId: location.id, type: newVote });
-                                };
-
-                                return (
-                                    <LocationOption
-                                        locationId={location.id}
-                                        address={location.address}
-                                        placeId={location.placeId}
-                                        lat={location.lat}
-                                        lng={location.lng}
-                                        onChange={handleChange}
-                                        vote={vote}
-                                        editable={editable}
-                                        participants={participants}
-                                        selectedParticipantId={selectedParticipant?.id}
-                                    />
-                                );
-                            }}
-                        />
-                    );
-                })}
+                                        return (
+                                            <tr className="group">
+                                                <td className="sticky left-0 z-10 h-12 bg-white px-4">
+                                                    <div className="flex items-center gap-x-2 text-sm">
+                                                        <MapPinIcon className="size-4 text-gray-500" />
+                                                        <div className="truncate">{location.address}</div>
+                                                    </div>
+                                                </td>
+                                                {participants.map((participant) => {
+                                                    const participantVote = getVote(participant.id, location.id);
+                                                    return (
+                                                        <td
+                                                            key={participant.id}
+                                                            className={cn(
+                                                                "h-12 border-l border-t",
+                                                                !participantVote || participantVote === "no" ? "bg-gray-100" : "bg-white",
+                                                            )}
+                                                        >
+                                                            <div className="flex items-center justify-center">
+                                                                <div
+                                                                    className={cn(
+                                                                        "inline-flex h-7 w-7 items-center justify-center rounded-full",
+                                                                        {
+                                                                            "bg-green-50": participantVote === "yes",
+                                                                            "bg-amber-50": participantVote === "ifNeedBe",
+                                                                            "bg-gray-200": participantVote === "no",
+                                                                        },
+                                                                    )}
+                                                                >
+                                                                    {editable && selectedParticipant?.id === participant.id ? (
+                                                                        <VoteSelector
+                                                                            value={vote}
+                                                                            onChange={handleChange}
+                                                                        />
+                                                                    ) : (
+                                                                        <VoteIcon type={participantVote} />
+                                                                    )}
+                                                                </div>
+                                                            </div>
+                                                        </td>
+                                                    );
+                                                })}
+                                                <td className="bg-diagonal-lines border-l"></td>
+                                            </tr>
+                                        );
+                                    }}
+                                />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
